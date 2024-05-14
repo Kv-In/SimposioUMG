7b1cd8a795217083088fc656475ebd1f63e77925
Henry Gustavo X Az <azhenrygus@outlook.es>
2024-05-13 19:46:32 -0600

    Fix null error in products table when table is empty

--- resources/views/table-producto/index.blade.php
+++ resources/views/table-producto/index.blade.php
@@ -14,112 +14,114 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/stylecrud.css">
 
-    
+
 </head>
 
 <body>
-    <h1 class="text-center text-secondary font-weight-bold p-5">Actualizar Productos</h1>
+<h1 class="text-center text-secondary font-weight-bold p-5">Actualizar Productos</h1>
 
-    <div class="container">
-        <div class="table-responsive">
-            <div class="table-wrapper">
-                <div class="table-title">
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <h2>Productos <b>Simposio</b></h2>
-                        </div>
+<div class="container">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <h2>Productos <b>Simposio</b></h2>
+                    </div>
 
-                        <div class="col-xs-6">
-                            <a href="{{ route('table-productos.create') }}" class="btn btn-success" data-toggle="modal"><i
-                                    class="material-icons">&#xE147;</i> <span>Añadir Registro</span></a>
-                            
-                        </div>
+                    <div class="col-xs-6">
+                        <a href="{{ route('table-productos.create') }}" class="btn btn-success" data-toggle="modal"><i
+                                class="material-icons">&#xE147;</i> <span>Añadir Registro</span></a>
 
                     </div>
+
                 </div>
+            </div>
 
 
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>
-                                
-                            </th>
-                            <th>id</th>
-                            <th>Nombre</th>
-                            <th>imagen</th>
-                            <th>Descripcion</th>
-                            <th>Precio</th>
-                            <th>Opciones </th>
-                        </tr>
-                    </thead>
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>
+
+                    </th>
+                    <th>id</th>
+                    <th>Nombre</th>
+                    <th>imagen</th>
+                    <th>Descripcion</th>
+                    <th>Precio</th>
+                    <th>Opciones</th>
+                </tr>
+                </thead>
 
 
-                    <tbody>
-                        @foreach ($tableProductos as $tableProducto)
-                        <tr>
-                           
-                                <td></td>
-                                <td>
-                                    {{++$i}}
-                                </td>
+                <tbody>
+                @foreach ($tableProductos as $tableProducto)
+                    <tr>
 
-                                <td>
-                                    {{ $tableProducto->nombre }}
-                                </td>
+                        <td></td>
+                        <td>
+                            {{++$i}}
+                        </td>
 
-                                <td>
-                                    
-                                   <img style="width: 100px;" src="{{ $tableProducto->imagen }}" alt="">
-                                </td>
+                        <td>
+                            {{ $tableProducto->nombre }}
+                        </td>
 
-                                <td>
-                                    {{ $tableProducto->descripcion }}
-                                </td>
+                        <td>
 
-                                <td>
-                                    Q.{{ $tableProducto->precio }}
-                                </td>
-                              
-                                <td>
+                            <img style="width: 100px;" src="{{ $tableProducto->imagen }}" alt="">
+                        </td>
 
+                        <td>
+                            {{ $tableProducto->descripcion }}
+                        </td>
 
-                                    <form action="{{ route('table-productos.destroy', $tableProducto->id) }}"
-                                        method="POST">
-                                        <a class="btn btn-outline-primary "
-                                            href="{{ route('table-productos.show', $tableProducto->id) }}"><i
-                                                class="fa fa-fw fa-eye"></i> {{ __('') }}</a>
-                                        <a class="btn btn-outline-success"
-                                            href="{{ route('table-productos.edit', $tableProducto->id) }}"><i
-                                                class="fa fa-fw fa-edit"></i> {{ __('') }}</a>
-                                    </form>
-                                    <button type="submit" class="btn btn-outline-danger"
-                                        onclick="abrirModalConfirmacion()">
-                                        <i class="fa fa-fw fa-trash"></i> {{ __('') }}
-                                    </button>
-                                </td>
+                        <td>
+                            Q.{{ $tableProducto->precio }}
+                        </td>
 
-                                 
-                        </tr>
-
-                        </tr>
-                        @endforeach 
-                    </tbody>
+                        <td>
 
 
+                            <form action="{{ route('table-productos.destroy', $tableProducto->id) }}"
+                                  method="POST">
+                                <a class="btn btn-outline-primary "
+                                   href="{{ route('table-productos.show', $tableProducto->id) }}"><i
+                                        class="fa fa-fw fa-eye"></i> {{ __('') }}</a>
+                                <a class="btn btn-outline-success"
+                                   href="{{ route('table-productos.edit', $tableProducto->id) }}"><i
+                                        class="fa fa-fw fa-edit"></i> {{ __('') }}</a>
+                            </form>
+                            <button type="submit" class="btn btn-outline-danger"
+                                    onclick="abrirModalConfirmacion()">
+                                <i class="fa fa-fw fa-trash"></i> {{ __('') }}
+                            </button>
+                        </td>
+
+
+                    </tr>
+
+                    </tr>
+                @endforeach
+                </tbody>
+
+
+                @isset($tableProducto)
                     <!-- Delete Modal HTML -->
                     <div id="deleteEmployeeModal" class="modal fade">
                         <div class="modal-dialog">
                             <div class="modal-content">
-                                
-                                <form id="confirmDeleteForm"                            
-                                action="{{ route('table-productos.destroy', $tableProducto->id) }}" method="POST">
+
+                                <form id="confirmDeleteForm"
+                                      action="{{ route('table-productos.destroy', $tableProducto->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <div class="modal-header">
                                         <h4 class="modal-title">Eliminar Registro</h4>
                                         <button type="button" class="close" data-dismiss="modal"
-                                            aria-hidden="true">&times;</button>
+                                                aria-hidden="true">&times;
+                                        </button>
                                     </div>
                                     <div class="modal-body">
                                         <p>¿Estás seguro de que deseas eliminar este registro?</p>
@@ -127,21 +129,23 @@
                                     </div>
                                     <div class="modal-footer">
                                         <input type="button" class="btn btn-default" data-dismiss="modal"
-                                            value="Cancelar">
+                                               value="Cancelar">
                                         <button type="submit" class="btn btn-danger">Eliminar</button>
                                     </div>
-                                </>
+                                </
+                                >
                             </div>
-                            
+
                         </div>
                     </div>
 
-                    <script>
-                        function abrirModalConfirmacion() {
-                            // Abrir el modal de confirmación
-                            $('#deleteEmployeeModal').modal('show');
-                        }
-                    </script>
+                @endisset
+                <script>
+                    function abrirModalConfirmacion() {
+                        // Abrir el modal de confirmación
+                        $('#deleteEmployeeModal').modal('show');
+                    }
+                </script>
 
 </body>
 
